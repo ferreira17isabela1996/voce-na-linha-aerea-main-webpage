72ce9254c6d46f35f189c3ad0f19c7989998e6fc
Isabela Ferreira <ifdigitalfly@gmail.com>
2025-09-06 15:02:23 -0300

    menu responsivo

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const toggle = document.getElementById("menu-toogle-id");
+/* const toggle = document.getElementById("menu-toogle-id");
 const navLinks = document.getElementById("nav-links");
 
 toggle.addEventListener("click", () => {
     console.log("estou funcionando")
   navLinks.classList.toggle("show");
 });
+ */
 
+
+/* SEÇÃO DE FAQ - ABRIR E FECHAR RESPOSTA */
+
+const pergunta = document.querySelectorAll(".question");
+pergunta.forEach((pergunta) => {
+  pergunta.addEventListener("click", () => {
+    const resposta = pergunta.nextElementSibling;
+    resposta.classList.toggle("show");
+  });
+});
